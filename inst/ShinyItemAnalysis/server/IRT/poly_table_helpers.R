06f6bb3c138f3c7531fd465b47e5884a5950706e
Josh McGrane <drjoshmcgrane@gmail.com>
2026-04-15 11:32:37 +1000

    refactor(poly-table): scaffold helper file and source from IRT.R

---
+++ inst/ShinyItemAnalysis/server/IRT/poly_table_helpers.R
@@ -0,0 +1,14 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# POLYTOMOUS PARAMETER TABLE HELPERS ####
+# Pure functions used by IRT_poly_summary_coef_reactive.
+# Design contract: docs/superpowers/specs/2026-04-15-polytomous-table-redesign-design.md
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# Forward declarations - filled in by subsequent tasks.
+build_nrm_table       <- function(fit, use_irt) stop("not yet implemented")
+build_grm_table       <- function(fit) stop("not yet implemented")
+build_masters_table   <- function(fit, model) stop("not yet implemented")
+append_fit_stats      <- function(tab, fit, include_infit) stop("not yet implemented")
+italicise_colnames    <- function(tab) stop("not yet implemented")
+strip_katex           <- function(tab) stop("not yet implemented")
+rsm_threshold_ses_delta <- function(fit) stop("not yet implemented")
